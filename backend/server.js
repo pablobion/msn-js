f63fb5dc70e0c9ca2a9a3cd8e7aa3fd8365b111d
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2021-01-05 17:16:20 -0300

    fix origin socket heroku

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,14 @@ const port = process.env.PORT || 80;
 // const io = require("socket.io")(server);
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "msn-js.vercel.app",
-        methods: ["GET", "POST"],
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true,
+        };
+        res.writeHead(200, headers);
+        res.end();
     },
 });
 
