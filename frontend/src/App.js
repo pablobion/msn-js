c27441c21d815bd3badcbdd8130ab464812cc0d3
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2021-01-15 14:24:28 -0300

    ✨ test gitmoji

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,6 @@ function App() {
     return (
         <Container>
             <Home />
-            {/* teste */}
             {/* <Login /> */}
             {/* <UploadPhoto /> */}
             {/* <Crop /> */}
