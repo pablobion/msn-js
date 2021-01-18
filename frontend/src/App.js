bfc23b36af24501feb6bd44111eaa43fba40e84a
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2021-01-18 18:01:12 -0300

    ♻️ Move functions send message and draw att.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import Login from "./pages/login/index";
 import { useUser } from "./pages/context/allusers";
 import { socket } from "./configs/socket_export";
 
+import { sendmessage, drawAttention } from "./scripts/refs";
+
 function App() {
     const { userChats } = useUser();
 
@@ -24,32 +26,11 @@ function App() {
 
     useEffect(() => {
         socket.on("send client message text", ({ message, socketidUser, socketidPerson, chatopen }) => {
-            if (!chatRefText.current) return false;
-            const indexPersonChat = chatRefText.current.findIndex((elem) => elem.id === socketidPerson);
-            const indexUserChat = chatRefText.current.findIndex((elem) => elem.id === socketidUser);
-
-            if (chatRefText.current[indexUserChat]) chatRefText.current[indexUserChat].insertAdjacentHTML("beforeend", `<p id="chat-usarname">${socketidUser} diz:</p><p id="chat-textmessage">${message}</p>`);
-            if (chatRefText.current[indexPersonChat]) chatRefText.current[indexPersonChat].insertAdjacentHTML("beforeend", `<p id="chat-usarname">${socketidUser} diz:</p><p id="chat-textmessage">${message}</p>`);
-
-            if (chatopen === false) {
-                //verificando se o chat estava aberto, se caso não, deixa ele laranja
-                const indexPersonMultiChat = multiChatRef.current.findIndex((elem) => `${elem.id}` === `${socketidUser}-multichat`);
-                if (chatRefText.current[indexPersonMultiChat]) multiChatRef.current[indexPersonMultiChat].style = "background-color: orange;";
-            }
+            sendmessage({ chatRefText, chatRef, multiChatRef, message, socketidUser, socketidPerson, chatopen });
         });
 
         socket.on("Draw AttenAttention", (data) => {
-            const indexPersonMultiChat = multiChatRef.current.findIndex((elem) => `${elem.id}` === `${data}-multichat`);
-            const indexPersonChat = chatRefText.current.findIndex((elem) => elem.id === data);
-
-            if (multiChatRef.current[indexPersonMultiChat]) multiChatRef.current[indexPersonMultiChat].style = "background-color: tomato;animation: shake 0.5s;";
-
-            if (chatRef.current[indexPersonChat]) chatRef.current[indexPersonChat].style = "animation: shake 0.5s; top: 100px; left: 100px; z-index: 1";
-
-            setTimeout(() => {
-                if (multiChatRef.current[indexPersonMultiChat]) multiChatRef.current[indexPersonMultiChat].style = "background-color: transparent;";
-                if (chatRef.current[indexPersonChat]) chatRef.current[indexPersonChat].style = "animation: none; top: 100px; left: 100px;";
-            }, 500);
+            drawAttention({ chatRefText, chatRef, multiChatRef, data });
         });
     }, []);
 
--- frontend/src/scripts/refs.js
+++ frontend/src/scripts/refs.js
@@ -1,3 +1,31 @@
-export default function () {
-    return "oi";
-}
+import { socket, connect } from "../configs/socket_export";
+
+export const sendmessage = ({ chatRefText, chatRef, multiChatRef, message, socketidUser, socketidPerson, chatopen }) => {
+    if (!chatRefText.current) return false;
+    if (!socketidUser) return alert("Houve um erro ao enviar sua mensaem.");
+    const indexPersonChat = chatRefText.current.findIndex((elem) => elem.id === socketidPerson);
+    const indexUserChat = chatRefText.current.findIndex((elem) => elem.id === socketidUser);
+
+    if (chatRefText.current[indexUserChat]) chatRefText.current[indexUserChat].insertAdjacentHTML("beforeend", `<p id="chat-usarname">${socketidUser} diz:</p><p id="chat-textmessage">${message}</p>`);
+    if (chatRefText.current[indexPersonChat]) chatRefText.current[indexPersonChat].insertAdjacentHTML("beforeend", `<p id="chat-usarname">${socketidUser} diz:</p><p id="chat-textmessage">${message}</p>`);
+
+    if (chatopen === false) {
+        //verificando se o chat estava aberto, se caso não, deixa ele laranja
+        const indexPersonMultiChat = multiChatRef.current.findIndex((elem) => `${elem.id}` === `${socketidUser}-multichat`);
+        if (chatRefText.current[indexPersonMultiChat]) multiChatRef.current[indexPersonMultiChat].style = "background-color: orange;";
+    }
+};
+
+export const drawAttention = ({ chatRefText, chatRef, multiChatRef, data }) => {
+    const indexPersonMultiChat = multiChatRef.current.findIndex((elem) => `${elem.id}` === `${data}-multichat`);
+    const indexPersonChat = chatRefText.current.findIndex((elem) => elem.id === data);
+
+    if (multiChatRef.current[indexPersonMultiChat]) multiChatRef.current[indexPersonMultiChat].style = "background-color: tomato;animation: shake 0.5s;";
+
+    if (chatRef.current[indexPersonChat]) chatRef.current[indexPersonChat].style = "animation: shake 0.5s; top: 100px; left: 100px; z-index: 1";
+
+    setTimeout(() => {
+        if (multiChatRef.current[indexPersonMultiChat]) multiChatRef.current[indexPersonMultiChat].style = "background-color: transparent;";
+        if (chatRef.current[indexPersonChat]) chatRef.current[indexPersonChat].style = "animation: none; top: 100px; left: 100px;";
+    }, 500);
+};
