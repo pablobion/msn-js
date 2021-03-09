cfe58bbee610f68f3b82dfc5d32a7786442266c7
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2021-03-09 14:48:15 -0300

    🚧 ✨ Add new videos winks

--- frontend/src/pages/chat/components/chat/components/sendWinks/components/winks/index.js
+++ frontend/src/pages/chat/components/chat/components/sendWinks/components/winks/index.js
@@ -4,11 +4,25 @@ import { Container } from "./styles";
 
 //videos
 import dancingpig from "./videos/dancing_pig.mp4";
-import kisses from "./videos/kiss.mp4";
+import kiss from "./videos/kiss.mp4";
+import bola from "./videos/bola.mp4";
+import bow from "./videos/bow.mp4";
+import cartao from "./videos/cartao.mp4";
+import fartguy from "./videos/fartguy.mp4";
+import frog from "./videos/frog.mp4";
+import guitar_smash from "./videos/guitar_smash.mp4";
+import heartkey from "./videos/heartkey.mp4";
+import knock from "./videos/knock.mp4";
+import laughing_girl from "./videos/laughing_girl.mp4";
+import notes from "./videos/notes.mp4";
+import water_balloon from "./videos/water_balloon.mp4";
+import yawning_moon from "./videos/yawning_moon.mp4";
 
 let isplaying;
 
 const FancyWinks = React.forwardRef((props, ref) => {
+    const winks = ["dancingpig", "kiss", "bola", "bow", "cartao", "fartguy", "frog", "guitar_smash", "heartkey", "knock", "laughing_girl", "notes", "water_balloon", "yawing_moon"];
+
     const videoRef = useRef([]);
     const buttonStop = useRef();
     let size = 400;
@@ -45,7 +59,7 @@ const FancyWinks = React.forwardRef((props, ref) => {
             <button className="actions-buttons-play-winks" id={`play-wink-pig-${props.socketidPerson}`} onClick={() => handlePlayVideo(0)}>
                 wink1
             </button>
-            <button className="actions-buttons-play-winks" id={`play-wink-kisses-${props.socketidPerson}`} onClick={() => handlePlayVideo(1)}>
+            <button className="actions-buttons-play-winks" id={`play-wink-kiss-${props.socketidPerson}`} onClick={() => handlePlayVideo(1)}>
                 wink2
             </button>
             {/* fim dos botoes que so reproduzem */}
@@ -60,8 +74,13 @@ const FancyWinks = React.forwardRef((props, ref) => {
             </video>
 
             <video width={size} ref={(el) => (videoRef.current[1] = el)}>
-                <source src={kisses} type="video/mp4" />
+                <source src={kiss} type="video/mp4" />
             </video>
+            {/* {winks.map((elem, index) => {
+                <video width={size} ref={(el) => (videoRef.current[index] = el)}>
+                    <source src={eval(elem)} type="video/mp4" />
+                </video>;
+            })} */}
         </Container>
     );
 });
--- frontend/src/pages/chat/components/chat/components/sendWinks/index.js
+++ frontend/src/pages/chat/components/chat/components/sendWinks/index.js
@@ -3,8 +3,20 @@ import React from "react";
 import { Container } from "./styles";
 
 //images
-import dancingpigimage from "./assets/dancingpig.png";
-import kissesimage from "./assets/kisses.png";
+import dancingpig from "./assets/dancingpig.png";
+import kisses from "./assets/kisses.png";
+import bola from "./assets/bola.png";
+import bow from "./assets/bow.jpg";
+import cartao from "./assets/cartao.png";
+import fartguy from "./assets/fartguy.png";
+import frog from "./assets/frog.png";
+import guitar_smash from "./assets/guitar_smash.png";
+import heartkey from "./assets/heartkey.png";
+import knock from "./assets/knock.png";
+import laughing_girl from "./assets/laughing_girl.png";
+import notes from "./assets/notes.png";
+import water_balloon from "./assets/water_balloon.png";
+import yyawning_moon from "./assets/yawning_moon.png";
 
 //socket
 import { socket } from "../../../../../../configs/socket_export";
@@ -18,10 +30,10 @@ const SendWinks = (props) => {
         <>
             <Container visible={props.visible}>
                 <button>
-                    <img src={dancingpigimage} alt="image-pig" onClick={() => handleSendWink("pig")} />
+                    <img src={dancingpig} alt="image-pig" onClick={() => handleSendWink("pig")} />
                 </button>
                 <button>
-                    <img src={kissesimage} alt="image-pig" onClick={() => handleSendWink("kisses")} />
+                    <img src={kisses} alt="image-pig" onClick={() => handleSendWink("kisses")} />
                 </button>
             </Container>
         </>
